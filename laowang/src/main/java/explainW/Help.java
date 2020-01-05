9b9d9efece29e299310bcf2d1066dae94d28ebd8
wangxtx <1375569701@qq.com>
2020-01-05 18:25:23 +0800

    开始

---
+++ laowang/src/main/java/explainW/Help.java
@@ -0,0 +1,7 @@
+package explainW;
+
+public class Help {
+	public static void help() {
+		System.out.println("欢迎使用老王工具类,:本工具类有6大模块,分别是DateUtilW(时间类)   NumberUtilW(数字类) \r  RandomUtilW(随机数类) RegexUtilW(随机类)   StreamUtilW(txt文件读行类)  StringUtilW(字符串类)\r输入以上六大类可调各种方法,每个方法后面都有个booean类型参数\r选择true可以开启说明,如果您不明白那些方法是做什么的,就选择true开启说明看看吧。");
+	}
+}
